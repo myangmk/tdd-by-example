219f71c97049be3aca5081543c8644755bab28f9
myangmk <mihyang.gu@kurlycorp.com>
2024-09-24 00:30:02 +0900

    ch.15

--- src/test/java/money/Money.java
+++ src/test/java/money/Money.java
@@ -22,7 +22,7 @@ class Money implements Expression {
         return amount == money.amount && currency().equals(money.currency);
     }
 
-    Money times(int multiplier) {
+    Expression times(int multiplier) {
         return new Money(amount * multiplier, currency);
     }
 
@@ -36,7 +36,8 @@ class Money implements Expression {
         return amount + " " + currency;
     }
 
-    Expression plus(Money addend) {
+    @Override
+    public Expression plus(Expression addend) {
         return new Sum(this, addend);
     }
 
--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -50,4 +50,14 @@ class MoneyTest {
         Money result = bank.reduce(Money.franc(2), "USD");
         assertEquals(Money.dollar(1), result);
     }
+
+    @Test
+    void testMixedAddition() {
+        Expression fiveBucks = Money.dollar(5);
+        Expression tenFrancs = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
+        assertEquals(Money.dollar(10), result);
+    }
 }
